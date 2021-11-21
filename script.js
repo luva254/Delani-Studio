b0ca0c0681c415089bb95eed5aa040659292eaa6
luva/254 <godfrey.luvandale@student.moringaschool.com>
2021-11-22 00:04:33 +0300

    add all changes made in html and css and sript.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("smt").addEventListener("click",function(){
+    alert("Thank you for your feedback and we will reach you back in a while")
+})
+$(document).ready(function(){
+    $("#details").click(function(){
+        $("p10","p11").show
+    })
+})
+$(document).ready(function(){
+    $("#development").click(function(){
+        $("p10","p11").show
+    })
+})
+$(document).ready(function(){
+    $("#management").click(function(){
+        $("p10","p11").show
+    })
+})
